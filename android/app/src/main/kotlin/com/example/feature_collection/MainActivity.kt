6110c8b616fa3dcb30f4b8e69f633a97f0f5e9f2
Burvan <burvani@mail.ru>
2025-03-19 13:54:41 +0300

    lay down a clean architecture

---
+++ android/app/src/main/kotlin/com/example/feature_collection/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.feature_collection
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/app/feature_collection_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FeatureCollectionApp extends StatelessWidget {
+  const FeatureCollectionApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('App'),
+    );
+  }
+}
